844a879e2de6984b005889bde7062935dc8d7e37
MikhailOOOOO <mihaohegov@gmail.com>
2024-12-16 13:32:57 +0300

    first

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="script.js" defer></script>
+    <link rel="stylesheet" href="styles.css">
+    <title>todo app</title>
+</head>
+
+<body>
+    <section class="todo">
+        <div class="text-filed-wrapper">
+            <input type="text" data-text-field>
+            <button data-add-todo-btn>add todo</button>
+        </div>
+
+        <hr>
+        
+        <div class="container" data-todo-container></div>
+    </section>
+</body>
+
+</html>
---
+++ script.js
@@ -0,0 +1,53 @@
+const input = document.querySelector("[data-text-field]");
+
+const addTodoBtn = document.querySelector("[data-add-todo-btn]");
+
+const container = document.querySelector("[data-todo-container]");
+
+const todoList = JSON.parse(localStorage.getItem("todos")) || [];
+
+const saveToLocalStorage = (key = "todos") => {
+    localStorage.setItem(key, JSON.stringify(todoList));
+}
+
+addTodoBtn.addEventListener("click", () => {
+    if (input.value.trim()) {
+        todoList.push(input.value);
+        input.value="";
+
+        localStorage.setItem("todos", JSON.stringify(todoList));
+
+        container.innerHTML="";
+
+        render();
+    }
+})
+
+const createElement = (tagName, textContent) => {
+    const element = document.createElement(tagName);
+    element.textContent = textContent;
+    return element;
+}
+
+const removeTodo = (index) => {
+    todoList.splice(index, 1);
+    saveToLocalStorage();
+    render();
+}
+
+const render = () => {
+    container.innerHTML = "";
+    todoList.forEach((todo, index) => {
+        const todoElement = createElement("div", todo);
+        const removeBtn = createElement("burron", " ❌");
+
+        removeBtn.addEventListener("click", () => removeTodo(index))
+
+        todoElement.classList.add("todo-item");
+        todoElement.append(removeBtn);
+
+        container.append(todoElement);
+    })
+}
+
+render();
